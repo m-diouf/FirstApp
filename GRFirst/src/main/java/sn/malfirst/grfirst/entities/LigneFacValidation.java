f366a93f701299cf4c2c22b54000c916c1545adf
malick niang <malickniang13@outlook.fr>
2019-12-12 16:06:14 +0000

    modif entite

--- GRFirst/src/main/java/sn/malfirst/grfirst/entities/LigneFacValidation.java
+++ GRFirst/src/main/java/sn/malfirst/grfirst/entities/LigneFacValidation.java
@@ -1,5 +1,25 @@
 package sn.malfirst.grfirst.entities;
 
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
 public class LigneFacValidation {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne 
+	private Facture facture;
+	@ManyToOne
+	private Utilisateur responsable;
+	private Date dateValidation;
 
 }
